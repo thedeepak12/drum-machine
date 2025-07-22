67c4aba23c969999d4b5628209ef293cb0ab7c92
Deepak <206972401+thedeepak12@users.noreply.github.com>
2025-07-22 21:06:29 +0530

    Add audio clips

---
+++ src/assets/audio.js
@@ -0,0 +1,47 @@
+export const audioClips = [
+  {
+    keyTrigger: 'Q',
+    clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+    clipId: 'Heater 1',
+  },
+  {
+    keyTrigger: 'W',
+    clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+    clipId: 'Heater 2',
+  },
+  {
+    keyTrigger: 'E',
+    clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+    clipId: 'Heater 3',
+  },
+  {
+    keyTrigger: 'A',
+    clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+    clipId: 'Heater 4',
+  },
+  {
+    keyTrigger: 'S',
+    clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+    clipId: 'Clap',
+  },
+  {
+    keyTrigger: 'D',
+    clip: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+    clipId: 'Open HH',
+  },
+  {
+    keyTrigger: 'Z',
+    clip: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+    clipId: 'Kick n Hat',
+  },
+  {
+    keyTrigger: 'X',
+    clip: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+    clipId: 'Kick',
+  },
+  {
+    keyTrigger: 'C',
+    clip: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+    clipId: 'Closed HH',
+  },
+];
